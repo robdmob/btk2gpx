a82a9bc226a09d388eee76828ec43246480c8731
robdmob <rob@kmr.uk.com>
2022-06-12 23:28:34 +0100

    changed to using structs for data :)

--- btk2gpx.c
+++ btk2gpx.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <stdint.h>
 #include <time.h>
 #ifdef _WIN32
 #include <conio.h>
@@ -9,6 +8,26 @@
 
 const char gpxheader[] = "<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"yes\"?>\n<gpx version=\"1.1\" creator=\"btk2gpx\" xmlns=\"http://www.topografix.com/GPX/1/1\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd\" xmlns:ns3=\"http://www.garmin.com/xmlschemas/TrackPointExtension/v1\">\n<trk>\n<trkseg>\n";
 const unsigned char btkheader[]  = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x3d, 0x0a, 0x87, 0x40, 0xcd, 0x8c, 0x57, 0x44, 0x66, 0x66, 0xa6, 0x3f, 0x01, 0x00, 0x05, 0x00};
+
+struct btkstart {
+	unsigned char type;
+	unsigned short year;
+	unsigned char month;
+	unsigned char day;
+	unsigned char hour;
+	unsigned char minute;
+	unsigned char second;
+	char padding[15];
+};
+
+struct btkpoint {
+	unsigned char type;
+	char temperature;
+	unsigned short count;
+	int elevation;
+	double latitude;
+	double longitude;
+};
 
 FILE *btkfile;
 FILE *gpxfile;
@@ -40,7 +59,7 @@ int main(int argc, char *argv[]) {
 		exit(1);
 	}
 
-	for (uint8_t i = 0; i < sizeof(btkheader); i++) {
+	for (unsigned char i = 0; i < sizeof(btkheader); i++) {
 		if (fgetc(btkfile) != btkheader[i]) {
 			fprintf(stderr, "File \"%s\" not valid BTK file.\n", argv[1]);
 			fclose(btkfile);
@@ -81,25 +100,14 @@ int main(int argc, char *argv[]) {
 
 void writeHeader() {
 
-	fseek(btkfile, 1, SEEK_CUR);
+	fseek(btkfile, -1, SEEK_CUR);
 
-	uint16_t year;
-	fread(&year, sizeof(year), 1, btkfile);
+	struct btkstart start;
 
-	struct tm timeinfo;
-	timeinfo.tm_year = year - 1900;
-	timeinfo.tm_mon = fgetc(btkfile) - 1;
-	timeinfo.tm_mday = fgetc(btkfile);
-	timeinfo.tm_hour = fgetc(btkfile);
-	timeinfo.tm_min = fgetc(btkfile);
-	timeinfo.tm_sec = fgetc(btkfile);
-
-	timestamp = mktime(&timeinfo);
-
-	fseek(btkfile, 15, SEEK_CUR);
+	fread(&start, sizeof(start), 1, btkfile);
 
 	char filename[24];
-	strftime(filename, sizeof(filename), "%Y-%m-%d %H.%M.%S.gpx", &timeinfo);
+	sprintf(filename, "%d-%.2d-%.2d %.2d.%.2d.%.2d.gpx", start.year, start.month, start.day, start.hour, start.minute, start.second);
 
 	gpxfile = fopen(filename,"w");
 
@@ -113,6 +121,17 @@ void writeHeader() {
 
 	printf("Created file \"%s\".\n", filename);
 
+	struct tm timeinfo;
+	timeinfo.tm_year = start.year - 1900;
+	timeinfo.tm_mon = start.month - 1;
+	timeinfo.tm_mday = start.day;
+	timeinfo.tm_hour = start.hour;
+	timeinfo.tm_min = start.minute;
+	timeinfo.tm_sec = start.second;
+	timeinfo.tm_isdst = 0;
+
+	timestamp = mktime(&timeinfo);
+
 }
 
 void writeFooter() {
@@ -124,28 +143,23 @@ void writeFooter() {
 
 void writePoint() {
 
-	int8_t temp;
-	uint16_t count;
-	int32_t elev;
-	double lat, lon;
+	fseek(btkfile, -1, SEEK_CUR);
 
-	fread(&temp, sizeof(temp), 1, btkfile);
-	fread(&count, sizeof(count), 1, btkfile);
-	fread(&elev, sizeof(elev), 1, btkfile);
-	fread(&lat, sizeof(lat), 1, btkfile);
-	fread(&lon, sizeof(lon), 1, btkfile);
+	struct btkpoint point;
 
-	if((lat > -200) && (lon > -100)) {
+	fread(&point, sizeof(point), 1, btkfile);
 
-		time_t offset = timestamp + count * 2;
+	if((point.latitude > -200) && (point.longitude > -100)) {
+
+		time_t offset = timestamp + point.count * 2;
 
 		char timebuf[21];
 		strftime(timebuf, sizeof(timebuf), "%Y-%m-%dT%H:%M:%SZ", gmtime(&offset));
 
-		fprintf(gpxfile, "<trkpt lat=\"%.13f\" lon=\"%.13f\">\n", lat, lon);
-		fprintf(gpxfile, "<ele>%.1f</ele>\n<time>%s</time>\n", elev / 10.0, timebuf);
+		fprintf(gpxfile, "<trkpt lat=\"%.13f\" lon=\"%.13f\">\n", point.latitude, point.longitude);
+		fprintf(gpxfile, "<ele>%.1f</ele>\n<time>%s</time>\n", point.elevation / 10.0, timebuf);
 		fprintf(gpxfile, "<extensions>\n<ns3:TrackPointExtension>\n");
-		fprintf(gpxfile, "<ns3:atemp>%.1f</ns3:atemp>\n", temp / 2.0 + 20.0);
+		fprintf(gpxfile, "<ns3:atemp>%.1f</ns3:atemp>\n", point.temperature / 2.0 + 20.0);
 		fprintf(gpxfile, "</ns3:TrackPointExtension>\n</extensions>\n");
 		fprintf(gpxfile, "</trkpt>\n");
 
